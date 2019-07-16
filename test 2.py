eb84c3bdfb3bebac6bd572b7626247a56662edb8
pallaudayakarthik <52955518+pallaudayakarthik@users.noreply.github.com>
2019-07-16 18:39:34 +0530

    Create test 2.py

---
+++ test 2.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number of ur choice :"))
+if a%2==0:
+    print("your number is even")
+elif a<0:
+    print("your number is invalid")
+else:
+    print("other types of numbers")
